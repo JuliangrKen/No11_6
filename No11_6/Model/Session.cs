448e65da7f094ac30b764eb8af9eb25bae5d34ae
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-18 22:13:07 +0300

    Добавил модель сессии для пользователя

---
+++ No11_6/Model/Session.cs
@@ -0,0 +1,28 @@
+﻿namespace No11_6.Model
+{
+    /// <summary>
+    /// Класс, инкапсулирующий данные о сессии пользователя
+    /// </summary>
+    public class Session
+    {
+        /// <summary>
+        /// Тип функции, которую должен исполнять бот при вводе текста пользователем
+        /// </summary>
+        public FuncType UserFuncType;
+
+        /// <summary>
+        /// Перечисление возможных функций бота
+        /// </summary>
+        public enum FuncType : byte
+        {
+            /// <summary>
+            /// Получение суммы
+            /// </summary>
+            GetSum,
+            /// <summary>
+            /// Получение количества символов
+            /// </summary>
+            GetNumChars
+        }
+    }
+}
