5a98fe1a2285657ed028f5c32db0cca7ac2739aa
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-18 22:19:02 +0300

    Добавил классы для временного хранения сессий пользователей

---
+++ No11_6/Services/IStorage.cs
@@ -0,0 +1,12 @@
+﻿using No11_6.Model;
+
+namespace No11_6.Services
+{
+    public interface IStorage
+    {
+        /// <summary>
+        /// Получение сессии пользователя по ID чата
+        /// </summary>
+        Session GetSession(long chatId);
+    }
+}
---
+++ No11_6/Services/MemoryStorage.cs
@@ -0,0 +1,25 @@
+﻿using No11_6.Model;
+using System.Collections.Concurrent;
+
+namespace No11_6.Services
+{
+    public class MemoryStorage : IStorage
+    {
+        /// <summary>
+        /// Хранилище сессий
+        /// </summary>
+        private readonly ConcurrentDictionary<long, Session> sessions = new();
+
+        public Session GetSession(long chatId)
+        {
+            // Возвращаем сессию по ключу, если она существует
+            if (sessions.ContainsKey(chatId))
+                return sessions[chatId];
+
+            // Создаем и возвращаем новую, если такой не было
+            var newSession = new Session();
+            sessions.TryAdd(chatId, newSession);
+            return newSession;
+        }
+    }
+}
