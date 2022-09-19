46962e7255a497054874e65760b60ec665cd02b3
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-19 21:55:04 +0300

    Организовал контрольную точку в приложении

--- No11_6/Program.cs
+++ No11_6/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using No11_6;
+using No11_6.Configuration;
+using No11_6.Controllers;
+using No11_6.Services;
+using System.Text.Json;
+using Telegram.Bot;
+
+var host = new HostBuilder()
+    .ConfigureServices((hostContext, services) => ConfigureServices(services))
+    .UseConsoleLifetime()
+    .Build();
+
+Console.WriteLine("Сервис запущен");
+await host.RunAsync();
+Console.WriteLine("Сервис остановлен");
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddTransient<InlineKeyboardController>();
+    services.AddTransient<TextMessageController>();
+
+    // Регистрируем метод получения конфигурации бота
+    services.AddSingleton(GetBotConfig());
+    var botConfig = GetBotConfig();
+    // Регистрируем объект TelegramBotClient c токеном подключения
+    services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient(botConfig.Token ?? ""));
+    // Регистрируем постоянно активный сервис бота
+    services.AddHostedService<Bot>();
+    // Регистрируем сервис получения данных о сессии пользователя
+    services.AddSingleton<IStorage, MemoryStorage>();
+}
+
+BotConfig GetBotConfig()
+{
+    var json = File.ReadAllText($@"{Environment.CurrentDirectory}/Configuration/BotConfig.json"); // Достаём из конфига объект 
+    return JsonSerializer.Deserialize<BotConfig>(json) // Десериализуем его в BotConfig
+           ?? throw new ArgumentNullException(); // При неудаче вызываем ошибку нулевого аргумента
+}
