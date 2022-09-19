c31741fd21aa5f8aca71c5e65d535fa8bdb82e84
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-19 21:53:43 +0300

    Добавил базовую реализацию двух контроллеров

---
+++ No11_6/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,42 @@
+﻿using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using No11_6.Services;
+using static No11_6.Model.Session;
+
+namespace No11_6.Controllers
+{
+    public class InlineKeyboardController
+    {
+        protected ITelegramBotClient telegramBotClient;
+        protected IStorage memoryStorage;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            this.telegramBotClient = telegramBotClient;
+            this.memoryStorage = memoryStorage;
+        }
+
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+
+            if (callbackQuery?.Data == null)
+                return;
+
+            (FuncType funcType, string funcName) = callbackQuery.Data switch
+            {
+                "getsum" => (FuncType.GetSum, "получение суммы"),
+                _ => (FuncType.GetNumChars, "получение количества символов в сообщении")
+            };
+
+            // Обновление пользовательской сессии новыми данными
+            memoryStorage.GetSession(callbackQuery.From.Id).UserFuncType = funcType;
+
+            // Отправляем в ответ уведомление о выборе
+            await telegramBotClient.SendTextMessageAsync(callbackQuery.From.Id,
+                $"<b>Выбранная функция - {funcName}.{Environment.NewLine}</b>" +
+                $"{Environment.NewLine}Можно поменять в главном меню.", cancellationToken: ct, parseMode: ParseMode.Html);
+        }
+    }
+}
---
+++ No11_6/Controllers/TextMessageController.cs
@@ -0,0 +1,53 @@
+﻿using No11_6.Services;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using static No11_6.Model.Session;
+
+namespace No11_6.Controllers
+{
+    public class TextMessageController
+    {
+        protected ITelegramBotClient telegramBotClient;
+        protected IStorage memoryStorage;
+
+        public TextMessageController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            this.telegramBotClient = telegramBotClient;
+            this.memoryStorage = memoryStorage;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            // Проверка на команды
+            switch (message.Text)
+            {
+                case "/start":
+
+                    var buttons = new List<InlineKeyboardButton[]>
+                    {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData($"Получать сумму чисел" , $"getsum"),
+                            InlineKeyboardButton.WithCallbackData($"Получать количество символов" , $"getsumchar")
+                        }
+                    };
+
+                    await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"<b>Выбирите необходимую функцию.</b>", cancellationToken: ct, parseMode: ParseMode.Html, replyMarkup: new InlineKeyboardMarkup(buttons));
+                    return;
+            }
+
+            // Если не команды, то срабатывает один из вариантов функционала
+            switch(memoryStorage.GetSession(message.Chat.Id).UserFuncType)
+            {
+                case FuncType.GetSum:
+                    await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"Выполнена FuncType.GetSum", cancellationToken: ct);
+                    break;
+                default:
+                    await telegramBotClient.SendTextMessageAsync(message.Chat.Id, $"Выполнена FuncType.GetSumChar", cancellationToken: ct);
+                    break;
+            }
+        }
+    }
+}
