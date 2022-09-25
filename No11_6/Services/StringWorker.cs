74a373b64447c5c3ffd8852a74da9760a4c48d72
Владислав Кравченко <vladik-g2911shangyang@mail.ru>
2022-09-25 19:33:42 +0300

    Добавил комментарии

--- No11_6/Services/StringWorker.cs
+++ No11_6/Services/StringWorker.cs
@@ -1,12 +1,26 @@
 ﻿namespace No11_6.Services
 {
+    /// <summary>
+    /// Сервис для работы со строками
+    /// </summary>
     public class StringWorker : IStringWorker
     {
+        /// <summary>
+        /// Возвращает количество символов
+        /// </summary>
+        /// <param name="str">строка</param>
+        /// <returns>количество символов</returns>
         public int GetNumberChars(string str)
         {
             return str.Length;
         }
 
+        /// <summary>
+        /// Возвращает сумму чисел в строке
+        /// </summary>
+        /// <param name="str">строка</param>
+        /// <param name="separatorForSplit">символ, служащий разделителем чисел друг от друга</param>
+        /// <returns>сумма чисел</returns>
         public int GetSumNumbersInString(string str, char separatorForSplit)
         {
             var numberArray = str.Split(separatorForSplit);
